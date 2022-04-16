b096d8bfa6fd91f340bcafc3d7e58400880a1f14
fixma-developers <102871351+fixma-developers@users.noreply.github.com>
2022-04-16 01:08:35 -0500

    Add files via upload

---
+++ js/scroll_reveal_english/index.js
@@ -0,0 +1,34 @@
+  window.sr = ScrollReveal();
+  sr.reveal('.title_services', {
+    duration: 1400,
+    origin: 'top',
+    distance: '0px'
+  });
+  
+  window.sr = ScrollReveal();
+  sr.reveal('.title_desarrollo', {
+    duration: 1400,
+    origin: 'top',
+    distance: '0px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.title_desarrollo', {
+    duration: 1400,
+    origin: 'top',
+    distance: '0px'
+  });
+
+   window.sr = ScrollReveal();
+  sr.reveal('.title_acerca_de', {
+    duration: 1400,
+    origin: 'top',
+    distance: '0px'
+  });
+
+
+
+
+
+
+
